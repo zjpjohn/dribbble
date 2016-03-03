68d53672f4a174bc6cddbd17c3561c48bd630f12
tangshiba <503976680@qq.com>
2016-03-03 23:24:57 +0800

    libs

--- dribbble/src/main/java/com/tangshiba/dribbble/ui/adapter/ShotAdapter.java
+++ dribbble/src/main/java/com/tangshiba/dribbble/ui/adapter/ShotAdapter.java
@@ -1,12 +1,14 @@
 package com.tangshiba.dribbble.ui.adapter;
 
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.agilie.dribbblesdk.domain.Shot;
+import com.tangshiba.dribbble.R;
 
 import java.util.List;
 
@@ -20,11 +22,13 @@ public class ShotAdapter extends RecyclerView.Adapter<ShotAdapter.ShotViewHolder
 
     @Override
     public ShotViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_item, null);
+        return new ShotViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ShotViewHolder holder, int position) {
+        Shot shot = mShots.get(position);
 
     }
 
@@ -36,11 +40,18 @@ public class ShotAdapter extends RecyclerView.Adapter<ShotAdapter.ShotViewHolder
     class ShotViewHolder extends RecyclerView.ViewHolder {
 
         ImageView mImageView;
-
+        TextView mViewsCount;
+        TextView mCommentsCount;
+        TextView mLikesCount;
 
         public ShotViewHolder(View itemView) {
             super(itemView);
+            mImageView = (ImageView) itemView.findViewById(R.id.image_view);
+            mViewsCount = (TextView) itemView.findViewById(R.id.views_count);
+            mCommentsCount = (TextView) itemView.findViewById(R.id.comments_count);
+            mLikesCount = (TextView) itemView.findViewById(R.id.likes_count);
         }
+
     }
 
 }
